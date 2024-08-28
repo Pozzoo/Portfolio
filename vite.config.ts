f2882b6bb5affc2ebae5ea8e447c2d855a63d941
Artur <arturpozzocmi@gmail.com>
2024-08-28 09:52:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://saram7619.github.io/deploying-vite-project-example/",
+  base: "./",
 })
